ae4d06ee582028b5ee4ed7caaa7d1c15f8461acd
jazzespinosa <jazel.espinosa96@gmail.com>
2021-03-23 22:09:52 +0800

    update running

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const db = mysql.createConnection({
   // user: "adminUser",
   // password: "house123",
   // database: "lpulabdb",
-  host: process.env.HOSTNAME,
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
-  database: "ebdb",
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT,
+  database: process.env.RDS_DB_NAME,
 });
 
 app.get("/", (req, res) => {
